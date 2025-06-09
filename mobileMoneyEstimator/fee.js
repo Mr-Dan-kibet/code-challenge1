f16b60707dae5fa5e2267d728d0cddc4a796ac5c
Mr-Dan-kibet <dan.rotich@student.moringaschool.com>
2025-06-09 16:44:10 +0300

    transaction fee calculation

---
+++ mobileMoneyEstimator/fee.js
@@ -0,0 +1,19 @@
+//Decleare function
+function estimateTransactionFee(amountToSend) {
+  let transactionFee = amountToSend * 0.015;
+  if (transactionFee < 10) {
+    transactionFee = 10;
+  } else if (transactionFee > 70) {
+    transactionFee = 70;
+  }
+  const totalDebited = amountToSend + transactionFee;
+
+  //log
+  console.log(`Sending KES ${amountToSend}:`);
+  console.log(`Calculated Transaction Fee: KES ${transactionFee}`);
+  console.log(`Total amount to be debited: KES ${totalDebited}`);
+  console.log(`Send money securly!`);
+}
+//prompt
+const money = Number(prompt(`Helllo! How much will you be sending today?`));
+estimateTransactionFee(money);
